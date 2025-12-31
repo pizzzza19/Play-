18ba13259a36b2b6d935baed2e9ac9eafd264033
Dmamss <129612706+Dmamss@users.noreply.github.com>
2026-01-01 00:07:11 +0100

    Update JitMemory.cpp

--- Source/ui_ios/JIT/JitMemory.cpp
+++ Source/ui_ios/JIT/JitMemory.cpp
@@ -1,16 +1,13 @@
 #include "JitMemory.h"
 #include "JitManager.h"
-#include "JitConfig.h"
 
 static uint8_t* g_execBase = nullptr;
 static size_t g_offset = 0;
 
 void JitMemory::Initialize() {
-#if PLAY_IOS_JIT
-    if (JitManager::Get().Initialize(PLAY_JIT_REGION_SIZE)) {
+    if (JitManager::Get().Initialize(64 * 1024 * 1024)) {
         g_execBase = (uint8_t*)JitManager::Get().GetExecBase();
     }
-#endif
 }
 
 uint8_t* JitMemory::Allocate(size_t size) {
@@ -21,15 +18,9 @@ uint8_t* JitMemory::Allocate(size_t size) {
 }
 
 uint8_t* JitMemory::GetWritePtr(uint8_t* execPtr) {
-#if PLAY_IOS_JIT
     return (uint8_t*)JitManager::Get().GetWritePtr(execPtr);
-#else
-    return execPtr;
-#endif
 }
 
 void JitMemory::Flush(uint8_t* execPtr, size_t size) {
-#if PLAY_IOS_JIT
     JitManager::Get().Invalidate(execPtr, size);
-#endif
 }
