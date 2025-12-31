f36c9baa66329d982b3e3fc8d9d7702c3c3466e5
Dmamss <129612706+Dmamss@users.noreply.github.com>
2025-12-31 21:00:24 +0100

    Update DualMapping.h

--- Source/ui_ios/JIT/DualMapping.h
+++ Source/ui_ios/JIT/DualMapping.h
@@ -1,6 +1,6 @@
 //
 //  DualMapping.h
-//  Play! - Dual Mapping JIT Implementation
+//  Play! - Dual Mapping JIT for iOS 26
 //
 
 #import <Foundation/Foundation.h>
@@ -9,38 +9,25 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface DualMapping : NSObject
-
-// Structure pour stocker les mappings
 typedef struct {
-    void* rwAddress;  // Read-Write address
-    void* rxAddress;  // Read-Execute address
+    void* rwAddress;
+    void* rxAddress;
     size_t size;
     int fd;
 } DualMappedRegion;
 
-// Initialisation
+@interface DualMapping : NSObject
+
 + (instancetype)sharedInstance;
 
-// Créer une région dual-mapped
 - (DualMappedRegion*)createDualMappedRegionWithSize:(size_t)size;
-
-// Libérer une région
 - (void)freeDualMappedRegion:(DualMappedRegion*)region;
-
-// Écrire du code dans la région RW
 - (BOOL)writeCode:(const void*)code 
            length:(size_t)length 
          toRegion:(DualMappedRegion*)region 
            offset:(size_t)offset;
-
-// Obtenir le pointeur RX pour l'exécution
 - (void*)getExecutablePointer:(DualMappedRegion*)region offset:(size_t)offset;
-
-// Vérifier si JIT est disponible
 - (BOOL)isJITAvailable;
-
-// Activer le mode debug (nécessaire pour iOS 26)
 - (BOOL)enableDebugMode;
 
 @end
