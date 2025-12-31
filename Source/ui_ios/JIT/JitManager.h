21656c7f21d74bf21333be8677fb791f07f94f99
Dmamss <129612706+Dmamss@users.noreply.github.com>
2026-01-01 00:04:57 +0100

    Update JitManager.h

--- Source/ui_ios/JIT/JitManager.h
+++ Source/ui_ios/JIT/JitManager.h
@@ -1,24 +1,23 @@
-#pragma once
+#ifndef JIT_MANAGER_H
+#define JIT_MANAGER_H
 
-#include <cstddef>
-#include <cstdint>
+#include <stddef.h>
+#include <stdint.h>
 
 class JitManager {
 public:
     static JitManager& Get();
-
     bool Initialize(size_t size);
-
-    void* GetExecBase() const;
     void* GetWritePtr(void* execPtr);
-
+    void* GetExecBase() const;
     void Invalidate(void* execPtr, size_t size);
 
 private:
     JitManager() = default;
     ~JitManager();
-
     void* m_rx = nullptr;
     void* m_rw = nullptr;
     size_t m_size = 0;
 };
+
+#endif
